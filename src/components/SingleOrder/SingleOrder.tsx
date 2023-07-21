d263e3c871f2b37583bb0a49be1335cf8f9c1872
dmlyrae <dm.lyrae@ya.ru>
2023-07-21 18:36:19 +0500

    test 2

--- src/components/SingleOrder/SingleOrder.tsx
+++ src/components/SingleOrder/SingleOrder.tsx
@@ -36,64 +36,81 @@ export const SingleOrder: React.FunctionComponent<IOrderProps> = ({ order, statu
 
 	return (
 			<article 
-				className={cl.item}
+				className={[cl.root, "p-12"].join(" ")}
 				onClick={showOrderModalWindow}
 			>
 				<div className={cl.description}>
-				<p className={cl.number}>#{order.number}</p>
-				<p className={cl.date}>{getTimeFromTimestamp(order.createdAt)}</p>
-				</div>
-				<h3 className={cl.title} style={status ? { marginBottom: 0 } : { marginBottom: 24 }}>{order.name}</h3>
-				{
-				status && (
-					<p className={cl.status} style={order.status === 'done' ? { color: '#00CCCC' } : order.status === 'done' ? { color: '#F2F2F3' } : { color: 'FF0000' }}>
-					{order.status === 'done' ? 'Ready' : order.status === 'done' ? 'Preparing' : 'Cancled'}
+					<p className={cl.number}>
+						#{order.number}
 					</p>
-				)
+					<p className={cl.date}>
+						{getTimeFromTimestamp(order.createdAt)}
+					</p>
+				</div>
+
+				<h3 className={cl.title} style={status ? { marginBottom: 0 } : { marginBottom: 24 }}>
+					{order.name}
+				</h3>
+				{
+					status && (
+						<p className={cl.status} style={order.status === 'done' ? { color: '#00CCCC' } : order.status === 'done' ? { color: '#F2F2F3' } : { color: 'FF0000' }}>
+							{order.status === 'done' ? 'Ready' : order.status === 'done' ? 'Preparing' : 'Cancled'}
+						</p>
+					)
 				}
-				<div className={cl.info}>
-				<ul className={cl.list}>
-					<li key={uuidv4()} className={cl.ingredient} style={{ zIndex: 6 }}>
-					<img src={bun?.image} alt={bun?.name} className={cl.image} style={{ opacity: 0.6 }} />
-					</li>
-					{
-					visiableIngredients.map((i: TCard, index: number) => {
-						if (index < maxIngredientsShowed && i.type !== 'bun') {
-							return (
-								<li 
-									key={uuidv4()} 
-									className={cl.ingredient} 
-									style={{ zIndex: maxIngredientsShowed - index }}
-								>
-									<img src={i.image} alt={i.name} className={cl.image} />
-								</li>
-							)
-						} else if (index === maxIngredientsShowed && chosenIngredients.length === 6 && i.type !== 'bun') {
-							return (
-								<li 
-									key={uuidv4()} 
-									className={cl.ingredient} 
-									style={{ zIndex: maxIngredientsShowed - index }}
-								>
-									<img src={i.image} alt={i.name} className={cl.image} />
-								</li>
-							)
-						} else if (i.type !== 'bun') {
-							return (
-								<li 
-									key={uuidv4()} 
-									className={cl.ingredient} 
-									style={{ zIndex: maxIngredientsShowed - index }}
-								>
-									<img src={i?.image} alt={i?.name} className={cl.image} style={{ opacity: 0.6 }} />
-									<p className={cl.rest}>+{chosenIngredients.length - maxIngredientsShowed}</p>
-								</li>
-							)
+				<div className={cl["main-info"]}>
+					<ul className={cl["ingredient-list"]}>
+						<li 
+							className={cl["ingredient"]} 
+							key={uuidv4()} 
+						>
+							<img src={bun?.image} alt={bun?.name} className={cl.image} style={{ opacity: 0.6 }} />
+						</li>
+						{
+						visiableIngredients.map((i: TCard, index: number) => {
+							if (index < maxIngredientsShowed && i.type !== 'bun') {
+								return (
+									<li 
+										key={uuidv4()} 
+										className={cl.ingredient} 
+										style={{ zIndex: maxIngredientsShowed - index }}
+									>
+										<img src={i.image} alt={i.name} className={cl.image} />
+									</li>
+								)
+							} else if (index === maxIngredientsShowed && chosenIngredients.length === 6 && i.type !== 'bun') {
+								return (
+									<li 
+										key={uuidv4()} 
+										className={cl.ingredient} 
+										style={{ zIndex: maxIngredientsShowed - index }}
+									>
+										<img src={i.image} alt={i.name} className={cl.image} />
+									</li>
+								)
+							} else if (i.type !== 'bun') {
+								return (
+									<li 
+										key={uuidv4()} 
+										className={cl.ingredient} 
+										style={{ zIndex: maxIngredientsShowed - index }}
+									>
+										<img src={i?.image} alt={i?.name} className={cl.image} style={{ opacity: 0.6 }} />
+										<p className={cl.rest}>+{chosenIngredients.length - maxIngredientsShowed}</p>
+									</li>
+								)
+							}
+						})
 						}
-					})
-					}
-				</ul>
-				<div className={cl.sum}><p className={cl.price}>{price}<span className={cl.icon}><CurrencyIcon type="primary" /></span></p></div>
+					</ul>
+					<div className={cl.sum}>
+						<p className={cl.price}>
+							{price}
+							<span className={cl.icon}>
+								<CurrencyIcon type="primary" />
+							</span>
+						</p>
+					</div>
 				</div>
 			</article>
 	)
--- src/pages/feed/Feed.tsx
+++ src/pages/feed/Feed.tsx
@@ -1,10 +1,12 @@
 import cl from './Feed.module.css';
-import React from 'react';
+import { useEffect } from 'react';
 import { wsInit, wsActions } from "../../store/reducers/wssOrders"
 import { IOrder } from "../../types/orders";
 import Loader from '../../components/Loader/Loader';
 import { useAppDispatch, useAppSelector } from '../../types/redux';
 import { SingleOrder } from '../../components/SingleOrder/SingleOrder';
+import { useNavigate } from 'react-router-dom';
+import { routerConfig } from '../../utils/routerConfig';
 
 const OrdersFeed = () => {
 	const dispatch = useAppDispatch();
@@ -15,62 +17,113 @@ const OrdersFeed = () => {
 	const ordersCreated = orders.filter((order) => order.status === 'created');
 
 	const { total, totalToday, wsConnected } = useAppSelector( store => store.wssOrders );
+	const { isAuth } = useAppSelector( state => state.user )
 
-	React.useEffect(() => {
-		dispatch(wsInit());
+	const navigate = useNavigate();
+
+
+	useEffect(() => {
+		if (!isAuth) {
+			navigate(routerConfig.login.path ?? `/`)
+		} else {
+			dispatch(wsInit());
+		}
 		return () => {
-			dispatch(wsActions.onClose());
+			if (wsConnected) {
+				dispatch(wsActions.onClose());
+			}
 		}
 	}, []);
 
 	return (
-		<div className={cl.page}>
-			<h2 className={cl.title}>Real-time order feed</h2>
-			{
-				wsConnected ?
-					(
-						<section className={cl.section}>
-							<ul className={cl.orderList}>
-								{orders.map((o: IOrder) => (
-									<li key={o.number} className={cl.order}><SingleOrder order={o} status={false} /></li>
-								))}
-							</ul>
-							<div className={cl.container}>
-								<div className={cl.orders}>
-									<div className={cl.block}>
-										<p className={cl.ordersTitle}>Ready:</p>
-										<ul className={cl.numberList}>
-											{
-												ordersSuccess.map((order) => {
-													return (<li className={cl.number} style={{ color: '#00CCCC' }} key={order.number}>{order.number}</li>)
-												})
-											}
-										</ul>
+		<div className={cl["feed-page"]}>
+			<div className={cl["feed"]}>
+
+				<h2 className={cl.title}>
+					{'Лента заказов'}
+				</h2>
+
+				{
+					wsConnected ?
+						(
+							<section className={cl.section}>
+
+								<ul className={cl.orderList}>
+									{orders.map( (order: IOrder) => (
+										<li key={order.number} className={cl.order}>
+											<SingleOrder order={order} status={false} />
+										</li>
+										)
+									)}
+								</ul>
+
+								<div className={cl.container}>
+									<div className={cl.orders}>
+										<div className={cl.block}>
+											<p className={cl.ordersTitle}>Ready:</p>
+											<ul className={cl.numberList}>
+												{
+													ordersSuccess.map((order) => {
+														return (<li className={cl.number} style={{ color: '#00CCCC' }} key={order.number}>{order.number}</li>)
+													})
+												}
+											</ul>
+										</div>
+										<div className={cl.block}>
+											<p className={cl.ordersTitle}>Preparing:</p>
+											<ul className={cl.numberList}>
+												{
+													ordersCreated.map((order) => {
+														return (<li className={cl.number} key={order.number}>{order.number}</li>)
+													})
+												}
+											</ul>
+										</div>
 									</div>
-									<div className={cl.block}>
-										<p className={cl.ordersTitle}>Preparing:</p>
-										<ul className={cl.numberList}>
-											{
-												ordersCreated.map((order) => {
-													return (<li className={cl.number} key={order.number}>{order.number}</li>)
-												})
-											}
-										</ul>
+									<div className={cl.summarize}>
+										<p className={cl.total}>Total burgers made:</p>
+										<p className={cl.sum}>{total}</p>
+									</div>
+									<div className={cl.summarize}>
+										<p className={cl.total}>Burgers made today:</p>
+										<p className={cl.sum}>{totalToday}</p>
 									</div>
 								</div>
-								<div className={cl.summarize}>
-									<p className={cl.total}>Total burgers made:</p>
-									<p className={cl.sum}>{total}</p>
-								</div>
-								<div className={cl.summarize}>
-									<p className={cl.total}>Burgers made today:</p>
-									<p className={cl.sum}>{totalToday}</p>
-								</div>
-							</div>
-						</section>
-					)
-					: <Loader />
-			}
+							</section>
+						)
+						: <Loader />
+				}
+			</div>
+			<div className={cl["stats"]} >
+
+				<div className={cl["stats__main"]}>
+					<div className={cl["stats__ready"]}>
+						<h4>{'Готовы:'}</h4>
+						<span>034533</span>
+						<span>034533</span>
+						<span>034533</span>
+						<span>034533</span>
+						<span>034533</span>
+					</div>
+					<div className={cl["stats__in-work"]}>
+						<h4>{'В работе:'}</h4>
+						<span>034533</span>
+						<span>034533</span>
+						<span>034533</span>
+						<span>034533</span>
+					</div>
+				</div>
+
+				<div className={cl["stats__all-time"]}>
+					<h2>{'Выполнено за все время:'}</h2>
+					<span>28752</span>
+				</div>
+
+				<div className={cl["stats__today"]}>
+					<h2>{'Выполнено за сегодня:'}</h2>
+					<span>752</span>
+				</div>
+			</div>
 		</div>
 	)
 };
