1c951bee84be78181294a07da496a3e1b49c1a91
dmlyrae <dm.lyrae@ya.ru>
2023-07-29 23:30:01 +0500

    Updates

--- src/types/redux.ts
+++
@@ -1,7 +0,0 @@
-import { AppDispatch } from "../store";
-import { TypedUseSelectorHook, useDispatch } from "react-redux";
-import { useSelector } from "react-redux";
-import { RootState } from "../store/reducers";
-
-export const useAppSelector:TypedUseSelectorHook<RootState> = useSelector;
-export const useAppDispatch = () => useDispatch<AppDispatch>();
